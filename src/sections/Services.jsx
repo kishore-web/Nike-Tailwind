75a97c9bd955acfef0a9abbaafad0371d962e9a4
Kishore Ch <kishu.559@gmail.com>
2023-09-26 03:10:33 +0530

    special offers

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -3,14 +3,16 @@ import { services } from "../constants";
 
 const Services = () => {
   return (
-    <section className="flex gap-10 max-container ">
+    <section className="flex gap-10 max-container flex-wrap max-sm:flex-col">
       {services.map((service) => (
-        <div className="flex flex-col flex-1 gap-5 justify-start shadow-3xl rounded-xl px-10 py-16">
-          <div className="bg-coral-red w-11 h-11 flex justify-center items-center rounded-full">
+        <div className="flex flex-col flex-1 gap-5 max-sm:gap-3 justify-start shadow-3xl rounded-xl px-10 py-16 sm:min-w-[350px] sm:w-[350px] max-sm:py-10">
+          <div className="bg-coral-red w-11 h-11 flex justify-center items-center rounded-full ">
             <img src={service.imgURL} alt="serviceImg" height={24} width={24} />
           </div>
-          <h3>{service.label}</h3>
-          <p>{service.subtext}</p>
+          <h3 className="text-3xl font-bold font-palanquin">{service.label}</h3>
+          <p className="text-xl text-gray-500 font-montserrat">
+            {service.subtext}
+          </p>
         </div>
       ))}
     </section>
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
-  )
-}
+    <section className="max-container flex gap-10 justify-between items-center max-xl:flex-col-reverse">
+      <div>
+        <img
+          src="https://nike-rosy.vercel.app/assets/offer-dd627fbb.svg"
+          alt="nike rosy image"
+        />
+      </div>
+      <div className="flex flex-col gap-5">
+        <h2>Special Offer</h2>
+        <p>
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p>
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="flex gap-5 pt-5">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button label="Learn more" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffers
+export default SpecialOffers;
